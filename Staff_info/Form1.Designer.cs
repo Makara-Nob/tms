2e6444e222bf53c5dd36de6a93e4eb9643def6bf
Makara <makarakara79@gmail.com>
2025-06-12 23:11:06 +0700

    Init project

---
+++ Staff_info/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Staff_info
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox10 = new TextBox();
+            textBox9 = new TextBox();
+            textBox8 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            textBox7 = new TextBox();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox10);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(30, 33);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(581, 510);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Information";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(186, 410);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(326, 27);
+            textBox10.TabIndex = 21;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(186, 364);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(326, 27);
+            textBox9.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(186, 314);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(326, 27);
+            textBox8.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(186, 271);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(326, 27);
+            textBox6.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(186, 176);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(326, 27);
+            textBox5.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(186, 228);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(326, 27);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(186, 131);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(326, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(186, 87);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(326, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(186, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(326, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(186, 456);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(98, 24);
+            checkBox2.TabIndex = 11;
+            checkBox2.Text = "Stop work";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(115, 456);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(65, 24);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "Work";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 410);
+            label10.Name = "label10";
+            label10.Size = new Size(52, 20);
+            label10.TabIndex = 9;
+            label10.Text = "Salary:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(6, 456);
+            label9.Name = "label9";
+            label9.Size = new Size(52, 20);
+            label9.TabIndex = 8;
+            label9.Text = "Status:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(6, 364);
+            label8.Name = "label8";
+            label8.Size = new Size(85, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Hired Date:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 317);
+            label7.Name = "label7";
+            label7.Size = new Size(121, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Contact Number:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 274);
+            label6.Name = "label6";
+            label6.Size = new Size(125, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Personal Number:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 228);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Address:";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 179);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 20);
+            label4.TabIndex = 3;
+            label4.Text = "BirthDate:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 134);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Gender:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Staff Name:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Staff ID:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Location = new Point(708, 33);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(436, 437);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Search:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(17, 102);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(401, 311);
+            dataGridView1.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(17, 43);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(401, 27);
+            textBox7.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(706, 493);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 31);
+            button1.TabIndex = 2;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1220, 628);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Staff_Information";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private Label label10;
+        private Label label9;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private TextBox textBox7;
+        private Button button1;
+    }
+}
